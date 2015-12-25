8969235aa78bd85e37384fa2bffb32a69554e1f1
bahadur khan <bahadurkhan@bahadurs-imac.home>
2015-12-25 23:50:15 +0300

    Initial Commit

---
+++ pokedax-by-devslopes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedax-by-devslopes
+//
+//  Created by bahadur khan on 25/12/2015.
+//  Copyright © 2015 bahadur khan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
